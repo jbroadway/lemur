20731fb98ac61157eb7482611973ed8cacc9c766
Johnny Broadway <johnny@johnnybroadway.com>
2012-08-22 13:19:35 -0500

    Added category renaming

--- handlers/category/edit.php
+++ handlers/category/edit.php
@@ -2,19 +2,21 @@
 
 $this->require_admin ();
 
+if (! isset ($_GET['id']) || ! is_numeric ($_GET['id'])) {
+	$this->redirect ('/lemur/admin');
+}
+
 if (! isset ($_GET['category']) || empty ($_GET['category'])) {
 	$this->redirect ('/lemur/admin');
 }
 
-$cat = new lemur\Category;
-$cat->owner = 0;
+$cat = new lemur\Category ($_GET['id']);
 $cat->title = $_GET['category'];
-$cat->sorting = $cat->next ('sorting');
 
 if (! $cat->put ()) {
 	$this->add_notification ('An error occurred.');
 } else {
-	$this->add_notification ('Category added.');
+	$this->add_notification ('Category updated.');
 }
 
 $this->redirect ('/lemur/admin');
--- js/admin.js
+++ js/admin.js
@@ -1,30 +1,50 @@
 var lemur = {
+	/**
+	 * Prompt for and save a new category.
+	 */
 	add_category: function () {
 		var category = prompt ('Category name:', '');
-		if (category.length > 0) {
+		if (category && category.length > 0) {
 			window.location.href = '/lemur/category/add?category=' + encodeURIComponent (category);
 		}
 		return false;
 	},
 	
-	edit_category: function (el) {
-		var id = $(el.target).data ('id');
-		console.log ('click: ' + id);
+	/**
+	 * Prompt for and update a category name.
+	 */
+	edit_category: function (e) {
+		var el = $(e.target),
+			id = el.data ('id'),
+			title = el.data ('title');
+
+		var category = prompt ('Category name:', title);
+		if (category && category.length > 0) {
+			window.location.href = '/lemur/category/edit?id=' + id + '&category=' + encodeURIComponent (category);
+		}
+		return false;
 	},
 	
-	on_category: function (el) {
-		var id = $(el).data ('id');
-		console.log ('on: ' + id);
+	/**
+	 * Hover change for category titles.
+	 */
+	on_category: function (e) {
+		$(e.target).parent ().addClass ('hovering');
 	},
 	
-	off_category: function (el) {
-		var id = $(el).data ('id');
-		console.log ('off: ' + id);
+	/**
+	 * Hover off for category titles.
+	 */
+	off_category: function (e) {
+		$(e.target).parent ().removeClass ('hovering');
 	}
 };
 
 $(function () {
-	$('.category')
+	/**
+	 * Initializations go here.
+	 */
+	$('.category-title')
 		.hover (lemur.on_category, lemur.off_category)
 		.click (lemur.edit_category);
 });
