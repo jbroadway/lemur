a345ab5c03c539faa480fb628cc0c9d708b87ace
Johnny Broadway <johnny@johnnybroadway.com>
2013-01-03 13:42:08 -0600

    Upgraded knockout.js to 2.2

--- handlers/course/content.php
+++ handlers/course/content.php
@@ -4,6 +4,26 @@ $this->require_admin ();
 
 $page->layout = 'admin';
 
+$c = new lemur\Course ($_GET['id']);
+if ($c->error) {
+	echo View::render ('lemur/admin/error', $c);
+	return;
+}
 
+$p = new lemur\Page ($_GET['page']);
+if ($p->error) {
+	echo View::render ('lemur/admin/error', $p);
+	return;
+}
+
+$page->title = __ ('Editing Page') . ': ' . $p->title;
+
+echo View::render (
+	'lemur/course/content',
+	array (
+		'course' => $c->id,
+		'page' => $p->id
+	)
+);
 
 ?>
