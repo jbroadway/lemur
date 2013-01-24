22c4d5ac5b0a741338ed92de98cbc169c597c0b4
Johnny Broadway <johnny@johnnybroadway.com>
2013-01-24 14:59:36 -0600

    Added course listings at /courses

--- handlers/index.php
+++ handlers/index.php
@@ -2,7 +2,31 @@
 
 $page->title = __ ($appconf['Lemur']['public_name']);
 $page->layout = $appconf['Lemur']['layout'];
+$page->add_script ('/apps/lemur/css/default.css');
 
-echo $tpl->render ('lemur/index', array ());
+// fetch sorted categories
+$categories = lemur\Category::sorted ();
+
+// fetch and sort courses by category (published, any owner)
+$courses = lemur\Course::categories (false, true);
+foreach (array_keys ($categories) as $k) {
+	$categories[$k] = (object) array (
+		'id' => $k,
+		'title' => $categories[$k],
+		'courses' => array (),
+		'course_count' => 0
+	);
+}
+foreach (array_keys ($courses) as $k) {
+	$categories[$courses[$k]->category]->courses[] = $courses[$k];
+	$categories[$courses[$k]->category]->course_count++;
+}
+
+echo $tpl->render (
+	'lemur/index',
+	array (
+		'categories' => $categories
+	)
+);
 
 ?>
