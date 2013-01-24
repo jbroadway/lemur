dbe2d8a5661605cc98d7a00140b28cf9f172d789
Johnny Broadway <johnny@johnnybroadway.com>
2013-01-24 14:59:26 -0600

    Added status to course fetching

--- models/Course.php
+++ models/Course.php
@@ -2,44 +2,46 @@
 
 namespace lemur;
 
-class Course extends \Model {
+use DB;
+use Model;
+
+class Course extends Model {
 	public $table = 'lemur_course';
 
 	/**
 	 * Get all courses grouped by category, optionally limited by owner.
 	 */
-	public static function categories ($owner = false) {
-		if ($owner) {
-			return \DB::fetch ('select
+	public static function categories ($owner = false, $published = false) {
+		$sql = 'select
 					lemur_course.*, lemur_category.title as category_title
 				from
 					lemur_course, lemur_category
 				where
-					lemur_course.category = lemur_category.id and
-					lemur_course.owner = ?
-				order by
-					lemur_category.sorting asc,
-					lemur_course.sorting asc',
-				$owner
-			);
+					lemur_course.category = lemur_category.id';
+
+		if ($owner) {
+			$sql .= ' and lemur_course.owner = ?';
 		}
-		return \DB::fetch ('select
-				lemur_course.*, lemur_category.title as category_title
-			from
-				lemur_course, lemur_category
-			where
-				lemur_course.category = lemur_category.id
-			order by
-				lemur_category.sorting asc,
-				lemur_course.sorting asc'
-		);
+
+		if ($published) {
+			$sql .= ' and lemur_course.status = 2';
+		}
+
+		$sql .= ' order by
+					lemur_category.sorting asc,
+					lemur_course.sorting asc';
+
+		if ($owner) {
+			return DB::fetch ($sql, $owner);
+		}
+		return DB::fetch ($sql);
 	}
 
 	/**
 	 * Get an associative array of pages for the current course.
 	 */
 	public function pages () {
-		return \DB::pairs (
+		return DB::pairs (
 			'select id, title from lemur_page where course = ? order by sorting asc',
 			$this->id
 		);
