9e3d96c965b7f9746a65134906e87baf61a53bc6
Johnny Broadway <johnny@johnnybroadway.com>
2013-01-24 13:33:27 -0600

    Updated Lemur::bootstrap()

--- lib/Lemur.php
+++ lib/Lemur.php
@@ -8,14 +8,15 @@ namespace lemur;
 class Lemur {
 	/**
 	 * Call this from Elefant's bootstrap.php file so that
-	 * links to `/course/` map to `/lemur/course/`.
+	 * links to `/course/` map to `/lemur/course/` and
+	 * links to `/courses` map to `/lemur/courses`.
 	 *
 	 * Usage:
 	 *
 	 *     lemur\Lemur::bootstrap ();
 	 */
 	public static function bootstrap () {
-		if (strpos ($_SERVER['REQUEST_URI'], '/course/') === 0) {
+		if (preg_match ('|^/courses?/?|', $_SERVER['REQUEST_URI'])) {
 			$_SERVER['REQUEST_URI'] = '/lemur' . $_SERVER['REQUEST_URI'];
 		}
 	}
