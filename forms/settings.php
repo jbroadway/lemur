e21061c8abce72aff1c2635a1fe7cf38ade99433
Johnny Broadway <johnny@johnnybroadway.com>
2013-06-19 15:50:44 -0500

    Added nav title to settings

--- forms/settings.php
+++ forms/settings.php
@@ -1,5 +1,9 @@
 ; <?php /*
 
+[title]
+
+not empty = 1
+
 [public_name]
 
 not empty = 1
--- handlers/settings.php
+++ handlers/settings.php
@@ -10,6 +10,7 @@ $page->title = __ ('Courses - Settings');
 $form = new Form ('post', $this);
 
 $form->data = array (
+	'title' => $appconf['Lemur']['title'],
 	'public_name' => $appconf['Lemur']['public_name'],
 	'layout' => $appconf['Lemur']['layout'],
 	'course_layout' => $appconf['Lemur']['course_layout'],
@@ -21,6 +22,7 @@ echo $form->handle (function ($form) {
 	if (! Ini::write (
 		array (
 			'Lemur' => array (
+				'title' => $_POST['title'],
 				'public_name' => $_POST['public_name'],
 				'layout' => $_POST['layout'],
 				'course_layout' => $_POST['course_layout'],
